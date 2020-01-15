499b1b70ff85b5fb4d2a817a57c066267f9bce6e
liuxin-5 <1073237144@qq.com>
2020-01-16 00:43:57 +0800

    前端基础代码

---
+++ ht-vue/src/view/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <el-button type="primary" @click="go">获取</el-button>
+    {{data}}
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+import qs from 'qs'
+export default {
+  data() {
+    return {
+      data: ''
+    }
+  },
+  methods: {
+    go() {
+      let saveParams = {
+          id: '1',
+        }
+      axios.post('/api/demo', this.qs.stringify(saveParams))
+      .then((response) => {
+          let ms = response.data
+          this.data = ms
+      }).catch((error) => {
+        console.log(error)
+      })
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
