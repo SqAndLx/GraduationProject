9c7105f167d7b5f67be80175c1094ca36dc1afb9
liuxin-5 <1073237144@qq.com>
2020-01-16 17:55:32 +0800

    redis

--- GraduationProject_Server/src/main/java/com/graduation/controller/UserController.java
+++ GraduationProject_Server/src/main/java/com/graduation/controller/UserController.java
@@ -2,8 +2,14 @@ package com.graduation.controller;
 
 import com.graduation.mode.User;
 import com.graduation.service.UserService;
+import com.redis.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
@@ -14,6 +20,27 @@ public class UserController {
 
     @PostMapping(path = "/demo")
     public User index(@RequestParam(name = "id") String id ) {
+        ApplicationContext context=new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+
+        RedisUtil redisUtil=(RedisUtil) context.getBean("redisUtil");
+
+        redisUtil.set("name", "how2java");
+        System.out.println(redisUtil.get("name"));
+//        redisUtil.del("name");
+        System.out.println(redisUtil.get("name"));
+
+        //=====================testNumber======================
+        long incr = redisUtil.incr("number", 1);
+        System.out.println(incr);
+        incr =redisUtil.incr("number", 1);
+        System.out.println(incr);
+
+        //=====================testMap======================
+        Map<String,Object> map=new HashMap<>();
+        map.put("name", "meepo");
+        map.put("pwd", "password");
+        redisUtil.hmset("user", map);
+        System.out.println(redisUtil.hget("user","name"));
         return userService.selectUserById(id);
     }
 }
