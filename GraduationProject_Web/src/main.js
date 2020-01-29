da528c563764f75fee0ece0f67a2198a5419e531
liuxin-5 <1073237144@qq.com>
2020-01-29 21:53:02 +0800

    登录代码提交

--- GraduationProject_Web/src/main.js
+++ GraduationProject_Web/src/main.js
@@ -5,6 +5,8 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import axios from 'axios'
 import qs from 'qs'
+import store from './store'
+
 Vue.prototype.$axios = axios
 Vue.prototype.qs = qs
 // Vue.config.productionTip = false
@@ -12,6 +14,7 @@ Vue.use(ElementUI)
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
--- GraduationProject_Web/src/router/index.js
+++ GraduationProject_Web/src/router/index.js
@@ -1,14 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import home from '@/views/Home'
-Vue.use(Router)
 
+import login from '@/views/login/login'
+import home from '@/views/user/home'
+Vue.use(Router)
 export default new Router({
   routes: [
     {
       path: '/',
+      name: 'login',
+      component: login
+    },
+    {
+      path: '/home',
       name: 'home',
       component: home
-    }
+    },
   ]
 })
