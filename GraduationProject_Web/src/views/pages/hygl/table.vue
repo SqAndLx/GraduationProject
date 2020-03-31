fb7254eb01ea4cdd75a09918f5fc7916c6797a31
shiqing <shiqing@thunisoft.com>
2020-03-31 23:11:13 +0800

    2

--- GraduationProject_Web/src/views/pages/hygl/table.vue
+++ GraduationProject_Web/src/views/pages/hygl/table.vue
@@ -1,4 +1,4 @@
-<template>
+<template >
   <el-table :data="tableData.slice((currentPage-1)*8,currentPage*8)" border>
     <el-table-column type="index" width="65" align="center"></el-table-column>
     <el-table-column prop="name" label="姓名" width="150" align="center"></el-table-column>
@@ -10,8 +10,8 @@
     <el-table-column prop="updateName" label="办理人" width="200" align="center"></el-table-column>
     <el-table-column prop="balance" label="账户余额" width="150" align="center"></el-table-column>
     <el-table-column label="操作" width="230" align="center">
-      <el-button type="primary" icon="el-icon-edit" circle @click="update"></el-button>
-      <el-button type="danger" icon="el-icon-delete" circle @click="delect"></el-button>
+      <el-button type="primary" icon="el-icon-edit"  circle @click="update()"></el-button>
+      <el-button type="danger" prop="id" icon="el-icon-delete"  circle @click="deleteHy(tableData[index].id)"></el-button>
     </el-table-column>
   </el-table>
 </template>
@@ -38,13 +38,44 @@
                 this.$message({
                     message: '修改',
                     type: 'warning'
-                })
+                });
+                // axios.post('/axios/api/deleteHy')
+                //     .then((response) => {
+                //         if (response.data.code == '200') {
+                //
+                //         }
+                //     }).catch((error) => {
+                //     console.log(error)
+                // })
             },
-            delect() {
-                this.$message({
-                    message: '确认删除吗？',
-                    type: 'warning'
-                })
+            deleteHy(val) {
+                    this.$confirm('将删除该会员, 是否继续?', '提示', {
+                        confirmButtonText: '确定',
+                        cancelButtonText: '取消',
+                        type: 'warning'
+                    }).then(() => {
+                        debugger
+                        let deleteParams = {
+                            id: val
+                        }
+                        axios.post('/axios/api/deleteHy',this.qs.stringify(deleteParams))
+                            .then((response) => {
+                                if (response.data.code == '200') {
+                                    this.$message({
+                                        message: '删除成功！',
+                                        type: 'success'
+                                    });
+                                }
+                            }).catch((error) => {
+                            console.log(error)
+                        })
+                    }).catch(() => {
+                        this.$message({
+                            type: 'info',
+                            message: '已取消删除'
+                        });
+                    });
+
             }
         },
         data() {
