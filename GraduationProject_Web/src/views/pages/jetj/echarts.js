d3b3212668115f5b2910c3c7587dfc6e480f95e3
shiqing <shiqing@thunisoft.com>
2020-04-03 18:32:51 +0800

    1

--- GraduationProject_Web/src/views/pages/jetj/echarts.js
+++ GraduationProject_Web/src/views/pages/jetj/echarts.js
@@ -1,43 +1,68 @@
-export default{
-    getOptions() {
-        let orgOptions = {
-            title: {
-                text: '某站点用户访问来源',
-                subtext: '纯属虚构',
-                left: 'center'
-            },
-            tooltip: {
-                trigger: 'item',
-                formatter: '{a} <br/>{b} : {c} ({d}%)'
-            },
-            legend: {
-                orient: 'vertical',
-                left: 'left',
-                data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
-            },
-            series: [
-                {
-                    name: '访问来源',
-                    type: 'pie',
-                    radius: '55%',
-                    center: ['50%', '60%'],
-                    data: [
-                        {value: 335, name: '直接访问'},
-                        {value: 310, name: '邮件营销'},
-                        {value: 234, name: '联盟广告'},
-                        {value: 135, name: '视频广告'},
-                        {value: 1548, name: '搜索引擎'}
-                    ],
-                    emphasis: {
-                        itemStyle: {
-                            shadowBlur: 10,
-                            shadowOffsetX: 0,
-                            shadowColor: 'rgba(0, 0, 0, 0.5)'
-                        }
-                    }
-                }
-            ]
-        };
-        return orgOptions
-      }
-    }
+export default {
+  getOptions() {
+    let orgOptions = {
+      title: {
+        text: '某站点用户访问来源',
+        subtext: '纯属虚构',
+        left: 'center'
+      },
+      tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b} : {c} ({d}%)'
+      },
+      legend: {
+        orient: 'vertical',
+        left: 'left',
+        data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+      },
+      series: [
+        {
+          name: '访问来源',
+          type: 'pie',
+          radius: '55%',
+          center: ['50%', '60%'],
+          data: [
+            {value: 335, name: '直接访问'},
+            {value: 310, name: '邮件营销'},
+            {value: 234, name: '联盟广告'},
+            {value: 135, name: '视频广告'},
+            {value: 1548, name: '搜索引擎'}
+          ],
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          }
+        }
+      ]
+    };
+    return orgOptions
+  },
+  getZzOptions() {
+    let zzOptions = {
+      legend: {},
+      tooltip: {},
+      dataset: {
+        dimensions: ['product', '2015', '2016', '2017'],
+        source: [
+          {product: 'Matcha Latte', '2015': 43.3, '2016': 85.8, '2017': 93.7},
+          {product: 'Milk Tea', '2015': 83.1, '2016': 73.4, '2017': 55.1},
+          {product: 'Cheese Cocoa', '2015': 86.4, '2016': 65.2, '2017': 82.5},
+          {product: 'Walnut Brownie', '2015': 72.4, '2016': 53.9, '2017': 39.1}
+        ]
+      },
+      xAxis: {type: 'category'},
+      yAxis: {},
+      // Declare several bar series, each will be mapped
+      // to a column of dataset.source by default.
+      series: [
+        {type: 'bar'},
+        {type: 'bar'},
+        {type: 'bar'}
+      ]
+    };
+    return zzOptions
+  }
+}
--- GraduationProject_Web/src/views/pages/jetj/jetj.vue
+++ GraduationProject_Web/src/views/pages/jetj/jetj.vue
@@ -1,33 +1,126 @@
 <template>
-<div class="all">
-  <div class="echarts">
-    <chart :options="orgOptions"></chart>
+  <div class="all" style="background: #f1f1f1;">
+    <!-- 概述 -->
+    <div class="gs-all">
+      <div class="gs">
+        <img src="../../../assets/gs.png" style="color: #2F80EC">
+        <span class="newTitle">概述</span>
+      </div>
+      <div class="wordkuang">
+        从{{this.tjq1}}到{{this.tjq2}}，美酷理发店共收入
+        <span style="cursor:text;color:#333;">10000</span>元，
+        其中，盈利
+        <span style="cursor:text;color:#333;">2546</span>元。
+      </div>
+    </div>
+    <!-- 柱状图-->
+    <div class="zzt-all">
+      <div class="zzt">
+        <img src="../../../assets/gs.png">
+        <span class="newTitle">近五月盈利、收入分析</span>
+      </div>
+      <p class="chartTitle" style="background-color:#f5f5f5">
+        <span style="float:right;margin-right:10px;">单位：元</span>
+      </p>
+      <div class="echartsZz">
+        <chart :options="zzOptions"></chart>
+      </div>
+    </div>
+    <!-- 饼状图-->
+    <div class="zzt-all">
+      <div class="zzt">
+        <img src="../../../assets/gs.png">
+        <span class="newTitle">员工业绩分析</span>
+      </div>
+      <p class="chartTitle" style="background-color:#f5f5f5">
+        <span style="float:right;margin-right:10px;">单位：件</span>
+      </p>
+      <div class="echartsBz">
+        <chart :options="orgOptions"></chart>
+      </div>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
-import echarts from './echarts.js'
-export default {
-  data() {
-    return {
-      orgOptions: {}
+    import echarts from './echarts.js'
+
+    export default {
+        data() {
+            return {
+                orgOptions: {},
+                tjq1: '',
+                tjq2: '',
+                zzOptions: {},
+            }
+        },
+        methods: {
+            getNowDate() {
+                this.tjq1 = new Date().getFullYear();
+                this.tjq2 = new Date().getFullYear() - 1;
+            }
+        },
+        created() {
+            this.getNowDate();
+        },
+        mounted() {
+            this.orgOptions = echarts.getOptions();
+            this.zzOptions = echarts.getZzOptions();
+        }
     }
-  },
-  methods: {
-  },
-  created(){
-    
-  },
-  mounted() {
-    this.orgOptions = echarts.getOptions()
-  }
-}
 </script>
 
 <style scoped>
-.all{
-  height: 100%;
-  width: 100%;
-}
+  .all {
+    height: 100%;
+    width: 100%;
+  }
+
+  .gs,
+  .zzt {
+    border-bottom: 3px solid #409EFF;
+    background: #f1f1f1;
+    padding: 15px;
+    text-align: left;
+  }
+
+  .gs img {
+    height: 20px;
+    padding-right: 5px;
+    width: 20px;
+  }
+
+  .zzt img {
+    height: 20px;
+    padding-right: 5px;
+    width: 20px;
+  }
+
+  .newTitle {
+    font-size: 20px;
+    color: #409EFF;
+    font-weight: bolder;
+  }
+
+  .wordkuang {
+    text-indent: 3em;
+    color: #333;
+    padding: 10px;
+    text-align: left;
+  }
+
+  .wordkuang span {
+    color: #409EFF;
+    cursor: pointer;
+  }
+
+  .gs-all,
+  .zzt-all {
+    margin: 10px 10px 0 10px;
+    background: #fff;
+  }
+
+  .zzt {
+
+  }
 </style>
